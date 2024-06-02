ed7681494940bc4a5a4cc79710b2044e1a965cdc
Edin <edyfer2@live.com.au>
2024-06-02 18:21:30 +0200

    Add if statements

--- main.rb
+++ main.rb
@@ -15,11 +15,14 @@ class Game
             #Declare variable called num_of_games and set to input
             num_of_games = gets.chomp
             p num_of_games
-            #If num_of_games is not a number or includes non-numeric string other than ., 
-            if num_of_games.match(/[^0-9.]/) || 
+            #If num_of_games is not a number or includes non-numeric string other than . and -, 
+            if num_of_games.match(/[^0-9.-]/) || 
             (num_of_games.match(/[0-9]/) && num_of_games.count(".") > 1) ||
             num_of_games == '' ||
-            num_of_games[num_of_games.length - 1] == "."
+            num_of_games[num_of_games.length - 1] == "." ||
+            num_of_games[num_of_games.length - 1] == '-' || 
+            num_of_games.match(/[0-9]-[0-9]/)  ||
+            num_of_games.count("-") > 1
                 #Display error "Not a number: Try Again"
                 puts "Not a number: Try Again"
             #If num_of_games is not an integer, but is a number, 
@@ -28,13 +31,16 @@ class Game
                 puts "Not a whole number: Try Again"
             #If num_of_games is an integer, but is less than 2 or is not even number,
             elsif num_of_games.match(/[0-9]/) && !num_of_games.include?(".")  &&
-                 num_of_games.to_i < 2 && num_of_games.to_i.odd? == true
+                 num_of_games.to_i < 2 || num_of_games.to_i.odd? == true
                 #Convert to integer
                 num_of_games = num_of_games.to_i
                 #Display error "Number has to be an even number at least 2: Try again"
                 puts "Number has to be an even number at least 2: Try Again"
             #If num_of_games is an integer and is even number at least 2,
+            else
                 #Convert to integer
+                num_of_games = num_of_games.to_i
+            end
         break if num_of_games.is_a?(Integer) == true && num_of_games >= 2 && num_of_games.even? == true
         end
     end
