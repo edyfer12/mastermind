913ee34f5fb6b876b9569d1f2677d994e7be6fea
Edin <edyfer2@live.com.au>
2024-06-03 13:46:28 +0200

    Indent lines of code

--- main.rb
+++ main.rb
@@ -101,8 +101,8 @@ class Game
         @codemaker = []
         #If human is a codemaker,
         if @human.player == 'codemaker'
-        #Output to the user "Please enter the four colours (red, orange, green, pink, brown, yellow):"
-        puts "Please enter the four colours (red, orange, green, pink, brown, yellow):"
+            #Output to the user "Please enter the four colours (red, orange, green, pink, brown, yellow):"
+            puts "Please enter the four colours (red, orange, green, pink, brown, yellow):"
         end
         #Create variable i and set to 0
         i = 0
