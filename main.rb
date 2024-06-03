431d08df6be6a1dcbee8efb76a768248705f59aa
Edin <edyfer2@live.com.au>
2024-06-03 11:35:08 +0200

    Add puts

--- main.rb
+++ main.rb
@@ -99,6 +99,7 @@ class Game
         #Create an array called codemaker
         @codemaker = []
         #Output to the user "Please enter the four colours (red, orange, green, pink, brown, yellow):"
+        puts "Please enter the four colours (red, orange, green, pink, brown, yellow):"
         #Create variable i and set to 0
         #Keep entering the colours four times as a codemaker
             #If the computer is a codemaker, invoke the method computer_make_code
